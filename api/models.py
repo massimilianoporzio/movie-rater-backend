873f809f33ca7b615dc41c893c23d54f05823c9b
Massimiliano <massimiliano.porzio@gmail.com>
2021-09-07 14:17:56 +0200

    creato modelli Movie e Rating

--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+User = get_user_model()
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=360)
+
+
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.PositiveIntegerField(
+        validators=[MinValueValidator(1),
+                    MaxValueValidator(5)] #1-5 stars
+    )
+
+    class Meta:
+        unique_together = (('user','movie'),)
+        index_together = (('user', 'movie'),) # OGNI RATING HA UN SOLO USER PER OGNI FILM.
+        # ( per ogni film, uno user può solo creare un voto)
