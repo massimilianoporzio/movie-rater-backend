d4b2a49aec6485cb9ff421f229ffd866f360b533
Massimiliano <massimiliano.porzio@gmail.com>
2021-09-07 14:02:23 +0200

    creato router per API

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter() #URLS PER REST API
 
 urlpatterns = [
-
+    path('',include(router.urls))
 ]
